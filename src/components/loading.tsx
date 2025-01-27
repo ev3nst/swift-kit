3b064a42948d0ce781dbd8b61a7554cc564416ad
ev3nst <futuregnr@gmail.com>
2025-01-28 02:06:12 +0300

    all lucide-react imports  are suffixed with Icon

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Loader } from 'lucide-react';
+import { LoaderIcon } from 'lucide-react';
 
 const Loading = () => {
 	const [loading, setLoading] = useState(true);
@@ -20,7 +20,7 @@ const Loading = () => {
 	return (
 		<div className="flex items-center justify-center min-h-screen">
 			<div className="text-center animate-pulse">
-				<Loader className="animate-spin w-10 h-10 text-foreground mx-auto mb-4" />
+				<LoaderIcon className="animate-spin w-10 h-10 text-foreground mx-auto mb-4" />
 			</div>
 		</div>
 	);
