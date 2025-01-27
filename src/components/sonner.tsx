e7eaadb2be02e65931857c8ab3c24a69eae00190
ev3nst <futuregnr@gmail.com>
2025-01-27 22:39:43 +0300

    sonner component prettify fix

--- src/components/sonner.tsx
+++ src/components/sonner.tsx
@@ -5,7 +5,7 @@ type ToasterProps = React.ComponentProps<typeof Sonner>;
 const Toaster = ({ ...props }: ToasterProps) => {
 	return (
 		<Sonner
-			theme='dark'
+			theme="dark"
 			className="toaster group"
 			toastOptions={{
 				classNames: {
