73e374f5533c2511868827f07d3734b10e86a741
ev3nst <futuregnr@gmail.com>
2025-02-03 01:43:18 +0300

    notes complete

---
+++ src/lib/models/note.ts
@@ -0,0 +1,81 @@
+import { db, type RawModel } from "@/lib/db";
+
+export class Note {
+	id!: number;
+	title: string;
+	content: string;
+	created_at!: string;
+	updated_at!: string;
+
+	private constructor(
+		id: number,
+		title: string,
+		content: string,
+		created_at: string,
+		updated_at: string,
+	) {
+		this.id = id;
+		this.content = content;
+		this.title = title;
+		this.created_at = created_at;
+		this.updated_at = updated_at;
+	}
+
+	static async get(id: number): Promise<Note> {
+		const result = await db.select<RawModel<Note>[]>(
+			'SELECT * FROM notes WHERE id = ?',
+			[id],
+		);
+
+		return new Note(
+			result[0].id,
+			result[0].title,
+			result[0].content,
+			result[0].created_at,
+			result[0].updated_at,
+		);
+	}
+
+	static async getAll(searchTerm: string = ''): Promise<Note[]> {
+		const query = searchTerm
+			? `SELECT id, title, created_at FROM notes WHERE id IN (SELECT docid FROM notes_fts WHERE notes_fts MATCH ?) ORDER BY id ASC`
+			: 'SELECT id, title, created_at FROM notes ORDER BY id ASC';
+
+		const results = await db.select<RawModel<Note>[]>(query, [searchTerm]);
+		return results.map(
+			rs =>
+				new Note(
+					rs.id,
+					rs.title,
+					rs.content,
+					rs.created_at,
+					rs.updated_at,
+				),
+		);
+	}
+
+	static async insert(data: Pick<Note, 'title' | 'content'>): Promise<Note> {
+		const result = await db.execute(
+			'INSERT INTO notes (title, content) VALUES (?, ?)',
+			[data.title, data.content],
+		);
+
+		if (result.lastInsertId) {
+			return (await Note.get(result.lastInsertId)) as Note;
+		} else {
+			console.error(result);
+			throw new Error('Error while insertion');
+		}
+	}
+
+	async update(): Promise<void> {
+		await db.execute(
+			'UPDATE notes SET title = ?, content = ? WHERE id = ?',
+			[this.title, this.content, this.id],
+		);
+	}
+
+	async delete(): Promise<void> {
+		await db.execute('DELETE FROM notes WHERE id = ?', [this.id]);
+	}
+}
--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { FilePlus2, FileX } from 'lucide-react';
 
 import {
@@ -15,37 +15,125 @@ import {
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/tooltip';
 import { Button } from '@/components/button';
 import { Tiptap } from '@/components/tiptap';
+import { Input } from '@/components/input';
 
-import { buttonVariants } from '@/lib/utils';
-import { Note } from '@/lib/db';
-
-import { NotesPagination } from './pagination';
+import { Note } from '@/lib/models/note';
+import { buttonVariants, cn } from '@/lib/utils';
 
 function Notes() {
+	const updateTimeoutRef = useRef<NodeJS.Timeout>();
 	const [notes, setNotes] = useState<Pick<Note, 'id' | 'title'>[]>([]);
-	const [content, setContent] = useState('');
-
-	const handleChange = newContent => {
-		setContent(newContent);
-	};
+	const [note, setNote] = useState<
+		Pick<Note, 'id' | 'title' | 'content' | 'created_at' | 'updated_at'>
+	>({} as any);
 
 	useEffect(() => {
 		(async () => setNotes(await Note.getAll()))();
 	}, []);
 
+	useEffect(() => {
+		if (typeof note.id === 'undefined' && notes.length > 0) {
+			(async () => {
+				const initialNote = await Note.get(notes[0].id);
+				setNote(initialNote);
+			})();
+		}
+	}, [note.id, notes]);
+
+	function debounceUpdateNote(note) {
+		if (updateTimeoutRef.current) clearTimeout(updateTimeoutRef.current);
+		updateTimeoutRef.current = setTimeout(async () => {
+			await (note as Note).update();
+		}, 500);
+	}
+
+	const handleEditorChange = newContent => {
+		note.content = newContent;
+		setNote(note);
+		debounceUpdateNote(note);
+	};
+
 	return (
-		<div className="flex flex-col">
-			<div className="mb-4 flex justify-between">
-				<div className="flex">
+		<div className="grid grid-cols-4 gap-4">
+			<div className="min-w-[250px] col-span-1 flex flex-col gap-2">
+				{notes
+					.sort((n, n2) => n.id - n2.id)
+					.map(item => (
+						<button
+							key={`notes_${item.id}`}
+							className={cn(
+								'flex items-center gap-2 rounded-lg border h-11 px-3 text-sm transition-all hover:bg-accent',
+								item.id === note?.id && 'bg-muted'
+							)}
+							onClick={async () => {
+								const fetchNote = await Note.get(item.id);
+								setNote(fetchNote);
+							}}
+						>
+							<div className="flex w-full flex-col gap-1">
+								<div className="flex items-center">
+									<div className="flex items-center gap-2">
+										<div
+											className={cn(
+												'ml-auto text-sm',
+												item.id === note?.id
+													? 'text-foreground'
+													: 'text-muted-foreground'
+											)}
+										>
+											{item.id === note.id
+												? note.title
+												: item.title}
+										</div>
+									</div>
+								</div>
+							</div>
+						</button>
+					))}
+			</div>
+			<div className="col-span-3">
+				<div className="flex gap-3 mb-4">
+					<Input
+						key={`note_title_${note.id}`}
+						className="h-11 flex-grow"
+						type="text"
+						name="title"
+						placeholder="Title for the note"
+						value={note.title}
+						spellCheck={false}
+						disabled={!note.id}
+						onChange={e => {
+							const findIndex = notes.findIndex(
+								ns => ns.id === note.id
+							);
+							notes[findIndex].title = e.currentTarget.value;
+							setNote({
+								...note,
+								title: e.currentTarget.value,
+								// @ts-ignore
+								update: note.update,
+							});
+							note.title = e.currentTarget.value;
+							debounceUpdateNote(note);
+						}}
+					/>
 					<Tooltip>
 						<TooltipTrigger asChild>
 							<Button
 								variant="outline"
 								size="icon"
-								className=" px-0 me-2"
-								onClick={() => console.log('create new')}
+								className="h-11 px-5"
+								onClick={async () => {
+									const newNote = await Note.insert({
+										title:
+											'New note - ' + (notes.length + 1),
+										content: 'Content...',
+									});
+									setNotes(await Note.getAll());
+									setNote(newNote);
+								}}
 							>
-								<FilePlus2 className="h-[1rem] w-[1rem]" />
+								<FilePlus2 className="h-8 w-8" />
 							</Button>
 						</TooltipTrigger>
 						<TooltipContent>
@@ -58,9 +146,12 @@ function Notes() {
 							<Button
 								variant="outline"
 								size="icon"
-								className=" px-0 me-2"
+								className={`h-11 px-5 ${
+									!note?.id ? 'disabled' : ''
+								}`}
+								disabled={!note?.id}
 							>
-								<FileX className="h-[1rem] w-[1rem]" />
+								<FileX className="h-8 w-8" />
 							</Button>
 						</AlertDialogTrigger>
 						<AlertDialogContent>
@@ -79,7 +170,21 @@ function Notes() {
 									className={buttonVariants({
 										variant: 'destructive',
 									})}
-									onClick={() => console.log('note remove')}
+									onClick={async () => {
+										await (note as Note).delete();
+										const notesList = await Note.getAll();
+										setNotes(notesList);
+										if (notesList.length > 0) {
+											setNote(
+												await Note.get(notesList[0].id)
+											);
+										} else {
+											setNote({
+												title: '',
+												content: '',
+											} as any);
+										}
+									}}
 								>
 									Delete
 								</AlertDialogAction>
@@ -87,9 +192,14 @@ function Notes() {
 						</AlertDialogContent>
 					</AlertDialog>
 				</div>
-				<NotesPagination items={notes} />
+				{note.id && (
+					<Tiptap
+						key={`note_tiptap_${note.id}`}
+						value={note.content}
+						onChange={handleEditorChange}
+					/>
+				)}
 			</div>
-			<Tiptap value={content} onChange={handleChange} />
 		</div>
 	);
 }
